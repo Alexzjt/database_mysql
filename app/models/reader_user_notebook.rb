23bfed88804f1e23891d75464826a020c334ee27
Alexzjt <1543042497@qq.com>
2016-01-16 14:49:44 +0800

    modify

--- app/models/reader_user_notebook.rb
+++ app/models/reader_user_notebook.rb
@@ -1,15 +1,15 @@
 class ReaderUserNotebook < ActiveRecord::Base
   def self.sort_list
     notebook_obj = ReaderUserNotebook.find(1)
-    notebook_list_array =Oj.load(notebook_obj.list)
+    notebook_list_hash =Oj.load(notebook_obj.list)
     book_id = ReaderUser.find(notebook_obj.user_id).book
     book_word_hash = ReaderPageWord.book_word_frequency_statistics(book_id)
     word_frequency_hash = WordFrequencyList.word_frequency_hash
-    array = notebook_list_array.keys
+    array = notebook_list_hash.keys
     array.sort! do |a, b|
       begin
-        time_a = Time.mktime(notebook_list_array[a])
-        time_b = Time.mktime(notebook_list_array[b])
+        time_a = Time.mktime(notebook_list_hash[a])
+        time_b = Time.mktime(notebook_list_hash[b])
         book_frequency_a = book_word_hash[a]
         book_frequency_b = book_word_hash[b]
         word_frequency_a = word_frequency_hash[a]
