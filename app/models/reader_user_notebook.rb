aac287a6f887ddb7395871f8a8cb47b8ffe09282
Alexzjt <1543042497@qq.com>
2016-01-18 10:11:57 +0800

    modify 'Date' to 'date'

--- app/models/reader_user_notebook.rb
+++ app/models/reader_user_notebook.rb
@@ -1,4 +1,5 @@
 class ReaderUserNotebook < ActiveRecord::Base
+  require 'date'
   def self.sort_list
     notebook_obj = ReaderUserNotebook.find(1)
     notebook_list_hash =Oj.load(notebook_obj.list)
@@ -8,15 +9,16 @@ class ReaderUserNotebook < ActiveRecord::Base
     array = notebook_list_hash.keys
     array.sort! do |a, b|
       begin
-        time_a = Time.mktime(notebook_list_hash[a])
-        time_b = Time.mktime(notebook_list_hash[b])
+        time_a = Date.parse(notebook_list_hash[a])
+        time_b = Date.parse(notebook_list_hash[b])
+        time_duration = (time_a-time_b).to_i
         book_frequency_a = book_word_hash[a]
         book_frequency_b = book_word_hash[b]
         word_frequency_a = word_frequency_hash[a]
         word_frequency_b = word_frequency_hash[b]
-        if time_a>time_b
+        if time_duration>0
           -1
-        elsif time_a<time_b
+        elsif time_duration<0
           1
         else
           if book_frequency_a > book_frequency_b
